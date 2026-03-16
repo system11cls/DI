d880a310afcbe47576f5dbabf34a816dd4b3e1d2
system1cls <d.faranosov@gmail.com>
2026-03-16 20:14:49 +0700

    tested singleton reproduce

--- src/test/java/DI_container/BeanData/BeansControllerTest.java
+++ src/test/java/DI_container/BeanData/BeansControllerTest.java
@@ -53,4 +53,43 @@ class BeansControllerTest {
         assertNotNull(car.engine);
         assertEquals(120, car.engine.horsepower);
     }
+
+    @Test
+    void testSingletone() throws ClassNotFoundException {
+        var infos = new HashMap<String, BeanInfo>();
+        var engineInfo = new BeanInfo();
+        engineInfo.name = "engine";
+        engineInfo.setters_args = List.of();
+        engineInfo.injected_classes = List.of();
+        engineInfo.interfacesImplemented = List.of();
+        engineInfo.constructor_args = List.of(
+                new ArgInfo("type","string", "XM1"),
+                new ArgInfo("horsepower","int", 120)
+        );
+        engineInfo.classPath = Engine.class.getCanonicalName();
+        engineInfo.scope = "singleton";
+        infos.put(engineInfo.name, engineInfo);
+
+        var carInfo = new BeanInfo();
+        carInfo.classPath = Car.class.getCanonicalName();
+        carInfo.constructor_args = List.of(
+                new ArgInfo("model","string", "XM1M"),
+                new ArgInfo("engine","engine", null)
+        );
+        carInfo.injected_classes = List.of(Engine.class.getCanonicalName());
+        carInfo.name = "car";
+        carInfo.interfacesImplemented = List.of();
+        carInfo.setters_args = List.of();
+        carInfo.scope = "singleton";
+        infos.put(carInfo.name, carInfo);
+
+        var controller = new BeansController(
+                infos, new ScopeFactoty(), new Metadata()
+        );
+
+        Car car = controller.getObject("car", Car.class);
+        Car car2 = controller.getObject("car", Car.class);
+        assertEquals(car2, car);
+
+    }
 }
